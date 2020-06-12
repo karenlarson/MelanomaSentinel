76402d0985f0059832561f19b8a480e1d8dcbcd9
karenlarson <Karen_Larson@brown.edu>
2020-06-12 08:20:36 -0400

    added % in each in each stage

--- sentinel_app.py
+++ sentinel_app.py
@@ -23,7 +23,7 @@ res = { 'DEPTH': 0, 'CS_EXTENSION': 0, 'MITOSES': 0, 'SEX': 0, 'ULCERATION': 0, 
 res['AGE'] = st.sidebar.number_input('Age of patient:', min_value= 0, max_value=120, value=40, step=10)
 res['DEPTH'] = 100*st.sidebar.number_input('Measured thickness (depth) of melanoma (in millimeters):', min_value = 0.0, max_value=9.80, value=1.00,step = 0.1)
 res['CS_EXTENSION'] = 100*(st.sidebar.number_input('Clark level (1 - 5): ', min_value = 1, max_value = 5, value = 1, step = 1) - 1)
-res['MITOSES'] = st.sidebar.number_input('Primary tumor mitotic count (# mitoses per square milimeter (mm)): ', min_value = 0, max_value = 11, value = 1, step = 1)
+res['MITOSES'] = st.sidebar.number_input('Primary tumor mitotic count (# mitoses per square milimeter (mm)): ', min_value = 0, max_value = 11, value = 0, step = 1)
 res['ULCERATION'] = 0 + (st.sidebar.text_input('Presence of ulceration (Yes or No):', 'No')[0].lower() == 'y')
 holder = st.sidebar.text_input('Sex:', 'Male')
 res['SEX'] = 1 + (holder[0].lower() == 'f')
@@ -53,28 +53,38 @@ sizes = y_proba_log
 #ax1.axis('equal')
 #plt.show()
 #st.pyplot()
+data = {}
 
 if res['DEPTH'] <= 100:
 	if res['MITOSES'] == 0 and res['ULCERATION'] == 0:
 		stage = "T1a"
+		percent = 0.43
+
 	else:
 		stage = "T1b"
+		percent = 2.69
 elif res['DEPTH'] <= 200:
 	if res['ULCERATION'] == 0:
 		stage = "T2a"
+		percent = 9.27
 	else:
 		stage = "T2b"
+		percent = 12.77
 elif res['DEPTH'] <= 400:
 	if res['ULCERATION'] == 0:
 		stage = "T3a"
+		percent = 16.24
 	else:
 		stage = "T3b"
+		percent = 20.70
 
 else:
 	if res['ULCERATION'] == 0:
 		stage = "T4a"
+		percent = 24.76
 	else:
 		stage = "T4b"
+		percent = 16.53
 
 pos_biops = "Positive Biopsy \n({:.2f}%)".format(sizes[0,1]*100)
 neg_biops = "Negative Biopsy \n({:.2f}%)".format(sizes[0,0]*100)
@@ -85,9 +95,21 @@ fig = plt.figure(FigureClass = Waffle, rows = 10, values = dat, icons = "child",
 #plt.show()
 st.pyplot()
 
+if y_proba_log[0,1] > 0.05 and y_proba_log[0,1] < 0.10:
+	st.subheader("This patient should be recommended for SLNB.")
+else:
+	st.subheader("This patient should not be recommended for SLNB.")
+
 st.write("This number shows the probability (or odds) that the melanoma has spread to the sentinel lymph node. For every 100 people similar to the patient, approximately {:.2f} would have a positive biopsy given the demographics. The sentinel lymph node is the first most likely lymph node for cancer to have spread. Rates of metastasis in the nodes vary greatly depending on several factors, including the depth of the melanoma and ulceration.".format(y_proba_log[0,1]*100))
 
-st.write(f"Based upon the Breslow Thickness, ulceration, and mitosis, the patient's tumor is classification {stage}. Patients with this classifications have had the following outcomes")
+
+
+st.write(f"Based upon the Breslow Thickness, ulceration, and mitosis, the patient's tumor is classification {stage}. Patients with this classifications have had the following outcomes:")
+
+
+
+st.write(f"{percent}% of {stage} have a poisitve SLNB.")
+
 
 #y_pred_gnb = gnb_clf.predict(x_trans)
 #y_proba_gnb = gnb_clf.predict_proba(x_trans)

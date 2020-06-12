8575c153fd9d437daeb8f6ec3a8a9cc5e55ab968
karenlarson <Karen_Larson@brown.edu>
2020-06-12 14:45:17 -0400

    removing duplicate source files

--- baseLine.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.base import BaseEstimator
-
-class BaseLine(BaseEstimator):
-    def fit(self, X, y=None):
-        pass
-    def predict(self,X):
-        return np.zeros((len(X),1))

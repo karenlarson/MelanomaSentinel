e1b861218bf71e74949199b3e54884c81d6205c1
karenlarson <Karen_Larson@brown.edu>
2020-06-17 14:01:42 -0400

    added roc_auc eval; made plots larger

--- classifierEvaluation.py
+++ classifierEvaluation.py
@@ -4,9 +4,12 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 def classifierEvaluation(clf, threshold, X_data, y_data, classifier_name, data_descriptor):
 	#creating predictions using classifier and a threshold for classifying as the positive class
 	y_pred = np.where(clf.predict_proba(X_data)[:,1] > threshold, 1, 0)
@@ -19,7 +22,9 @@ def classifierEvaluation(clf, threshold, X_data, y_data, classifier_name, data_d
 	recall = recall_score(y_data, y_pred)
 	precision = precision_score(y_data,y_pred)
 	specificity = tn/(tn + fp)
-	print("For the {} using {}, the accuracy is {:.4f}, the precision is {:.4f}, the recall is {:.4f}, the specificity is {:.4f} and the f1 score is {:.4f}".format(data_descriptor, classifier_name, score, precision, recall, specificity, f1))
+	roc_auc = roc_auc_score(y_data, clf.predict_proba(X_data)[:,1])
+
+	print("For the {} using {}, the accuracy is {:.4f}, the precision is {:.4f}, the recall is {:.4f}, the specificity is {:.4f}, the f1 score is {:.4f}, the auc is {:.4f}".format(data_descriptor, classifier_name, score, precision, recall, specificity, f1, roc_auc))
 
 
 	#creating a seaborn visualization for the confusion matrix; splitting the colors based upon the Negative/Positive true labels.
@@ -37,10 +42,17 @@ def classifierEvaluation(clf, threshold, X_data, y_data, classifier_name, data_d
 	#a sense of the specificty and recall of the method
 	relative_percents = np.array([[tn/(tn + fp), fp/(tn + fp)],[fn/(fn + tp), tp/(fn + tp)]])
 	print(cf_mat)
+	if data_descriptor == "Training Data":
+		colors = "Blues"
+	elif data_descriptor == "Validation Data":
+		colors = "Reds"
+	else:
+		colors = "Greens"
+	sns.heatmap(relative_percents, annot=labels, annot_kws={"size":20}, fmt='', cmap = colors)
+	plt.ylabel('True label', fontsize=18)
+	plt.xlabel('Predicted label', fontsize=18)
+	title = "Confusion Matrix for \n{} using {}".format(classifier_name, data_descriptor)
+	plt.title(title, fontsize=18)
+	plt.tick_params(axis='both', which='major', labelsize=15)
 
-	sns.heatmap(relative_percents, annot=labels, fmt='', cmap = 'Blues')
-	plt.ylabel('True label')
-	plt.xlabel('Predicted label')
-	title = "Confusion Matrix for {} using {}".format(classifier_name, data_descriptor)
-	plt.title(title)
 	plt.show()

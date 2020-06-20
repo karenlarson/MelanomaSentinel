ac037fe865aeb43edee5cbc83790829a89a908e6
karenlarson <Karen_Larson@brown.edu>
2020-06-20 16:42:28 -0400

    updated the model parameters

--- MelanomaSentinel.py
+++ MelanomaSentinel.py
@@ -5,7 +5,7 @@ from encodeAttribs import preprocess_pipeline
 from baseLine import BaseLine
 from sklearn.model_selection import cross_val_score
 import pickle 
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForest
 import pandas as pd
 import psycopg2 as sql
 from classifierEvaluation import classifierEvaluation
@@ -19,13 +19,13 @@ This is the main function for creating the melanoma prediction model. It calls f
 4) Calls preprocess_pipeline in encodeAttribs.py to create numerical features of degree 3
 and one-hot encode the cateogrical attributes
 5) Trains a baseline classifier (see BaseLine.py for details)
-6) Fits a logistic regression classifier using sklearn
+6) Fits a random forest classifier using sklearn
 7) Validates the model using classifierEvaluation.py
 8) Saves the model using pickle for the web-app's use
 """
 
 #cleaning the data in cleanData
-sql_mela = sql.connect(host="localhost", user="karenlarson", dbname="melanoma")
+sql_mela = sql.connect(dbname="melanoma")
 query = """SELECT "AGE", "SEX", "DEPTH", "ULCERATION", "MITOSES", "CS_EXTENSION", "PRIMARY_SITE", "CS_LYMPH_NODE_METS"
             FROM melanoma"""
 
@@ -43,8 +43,8 @@ mela = pd.read_sql_query(query, sql_mela)
 X, y = cleanData(mela)
 
 #split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=20)
-X_train, X_cv, y_train, y_cv = train_test_split(X_train, y_train, test_size=0.10, random_state=20)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y,random_state=20)
+X_train, X_cv, y_train, y_cv = train_test_split(X_train, y_train, test_size=0.125, stratify = y, random_state=20)
 
 #using functions so easily adjusted
 num_attribs = ['AGE','DEPTH', "CS_EXTENSION", "MITOSES"]
@@ -62,22 +62,17 @@ base_scores = cross_val_score(base_line_clf,x_train,y_train,cv=10,scoring="accur
 print(base_scores.mean())
 
 #fitting to a logistic regression classifier
-log_clf = LogisticRegression(solver="liblinear")
-log_clf.fit(x_train,y_train)
-
-# summarize feature importance
-importance = log_clf.coef_[0]
-for i,v in enumerate(importance):
-    print('Feature: %0d, Score: %.5f' % (i,v))
+forest_clf = RandomForestClassifier(bootstrap=True, max_features='sqrt', max_depth = 9, n_estimators=1200)
+forest_clf.fit(x_train,y_train)
 
 #evaluating the goodness of the logistic regression classifier
-classifierEvaluation(log_clf, 0.005, x_train, y_train, "Logistic Regression", "Training Data")
-classifierEvaluation(log_clf, 0.005, x_cv, y_cv, "Logistic Regression", "Validation Data")
-classifierEvaluation(log_clf, 0.005, x_test, y_test, "Logistic Regression", "Test Data")
+classifierEvaluation(forest_clf, 0.05, x_train, y_train, "Logistic Regression", "Training Data")
+classifierEvaluation(forest_clf, 0.05, x_cv, y_cv, "Logistic Regression", "Validation Data")
+classifierEvaluation(forest_clf, 0.05, x_test,y_test, "Logistic Regression", "Test Data")
 
 #dumps the trained model using pickle for the web-app to use.
-filename = 'logistic_clf.sav'
-pickle.dump(log_clf, open(filename, 'wb'))
+filename = 'forest_clf.sav'
+pickle.dump(forest_clf, open(filename, 'wb'))
 
 #dumps the feature pipeline using pickle for the web-app to use
 filename = 'transform.sav'

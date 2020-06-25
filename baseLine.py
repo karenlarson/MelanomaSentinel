fe854d353d672742cf4ee5e46bdf02ff0f0ea9a6
karenlarson <Karen_Larson@brown.edu>
2020-06-25 19:44:07 -0400

    removing repeated files

--- baseLine.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.base import BaseEstimator
-
-class BaseLine(BaseEstimator):
-	"""Creates a Base-Line estimator by estimating all zeros, which is the majority class in this case"""
-    def fit(self, X, y=None):
-        pass
-    def predict(self,X):
-        return np.zeros((len(X),1))
